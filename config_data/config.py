6177204da0b322a3efd6edf9ad63867156e09635
morsergey <workmsl@yandex.ru>
2022-11-02 23:24:28 +0300

    изменение

--- config_data/config.py
+++ config_data/config.py
@@ -6,10 +6,10 @@ if not find_dotenv():
 else:
     load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
-# BOT_TOKEN = os.getenv('5746583328:AAGWbrwSkrNRO7RKhPaJ3rmCKuxxCa7chIw')
-# RAPID_API_KEY = os.getenv('4be51f1e2cmsh6a32f721a78b683p1b0045jsn3beeed16815c')
+# BOT_TOKEN = os.getenv('BOT_TOKEN')
+# RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+BOT_TOKEN = os.getenv('5746583328:AAGWbrwSkrNRO7RKhPaJ3rmCKuxxCa7chIw')
+RAPID_API_KEY = os.getenv('4be51f1e2cmsh6a32f721a78b683p1b0045jsn3beeed16815c')
 
 DEFAULT_COMMANDS = (
     ('start', "Запустить бота"),
