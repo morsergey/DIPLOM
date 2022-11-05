2a1c30190cc0463a574ebd4aee629f522d982b10
morsergey <workmsl@yandex.ru>
2022-11-05 21:11:36 +0300

    изменение

--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -1,9 +1,11 @@
 from telebot.types import Message
-
 from loader import bot
-
-
 @bot.message_handler(commands=['start'])
 def bot_start(message: Message):
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}!")
+    welcome = ''' здравствуйте! Я бот "Too Easy Travel" и помогу Вам выбрать отель мечты.
+     География поиска - весь мир. Я покажу вам сайты и фоторгафйии понравившихся отелей
+     в соответствии с выбраными Вами характеристиками. Приступим.  
+    '''
+    bot.reply_to(message, f"{message.from_user.full_name} {welcome}")
+
 
